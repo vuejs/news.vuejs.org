c6e0c9ec146281e1435dab9c4ff0a88a45a78c37
Damian Dulisz <damian.dulisz@gmail.com>
2018-07-31 16:51:29 +0200

    Add tweets-as-stories support

---
+++ components/stories/TweetStory.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.story-tweet
+  Tweet(
+    :id="story.url"
+    :options="{ cards: 'hidden', linkColor: '#42b983' }"
+  )
+    Spinner
+</template>
+
+<script>
+import { Tweet } from 'vue-tweet-embed'
+import Spinner from '~/components/Spinner'
+
+export default {
+  components: { Tweet, Spinner },
+  props: {
+    story: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+
+.story-tweet
+  position: relative
+  min-height: 120px
+  margin-bottom: 20px
+</style>
